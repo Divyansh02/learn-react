b55c3f87eeef7f1b49b93c5f6e9ea9fcd8407ee5
DJ <divyansh.guddu@gmail.com>
2019-09-14 00:45:16 +0530

    test react-mastery | video player added

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -1,10 +1,26 @@
 import React from "react";
 
 const VideoDetails = props => {
-    if(!props.video){
-        return <div>Loading....</div>
-    }
-  return <div>{props.video.snippet.title}</div>;
+    console.log(props.video)
+  if (!props.video) {
+    return (
+      <div>Seacrh anything and Click on any video to display its title</div>
+    );
+  }
+
+  const videoSrc=`https://www.youtube.com/${props.video.id.videoId}`
+
+  return (
+    <div>
+        <div className="ui embed">
+            <iframe src={videoSrc}/>
+        </div>
+      <div className="ui segment">
+        <h4 className="ui header"> {props.video.snippet.title}</h4>
+        <p>{props.video.snippet.description} </p>
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetails;
